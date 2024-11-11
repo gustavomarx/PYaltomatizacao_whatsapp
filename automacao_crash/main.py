143d246747c3060160e08d3e199b4fcd24e1b6f1
gustavomarx <gustavomarx15@gmail.com>
2024-11-11 08:21:10 -0300

    Delete automacao_crash directory

--- automacao_crash/main.py
+++
@@ -1,7 +0,0 @@
-
-from openai import OpenAI
-client = OpenAI(api_key='sk-XYPRNRMbRuoFqesO3tsjCcSmwS6z6_SGUA53lSH33mT3BlbkFJSADWQHwZ05F9RuxMWXNZp_NGns3v3QQn_zYM0BHYsA')
-
-audio_file = open("c:/Users/gusta/OneDrive/Área de Trabalho/Dev Python/workspace/automacao_crash/audioteste.mp3", "rb")
-transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file,  response_format="text")
-print(transcription.text)
